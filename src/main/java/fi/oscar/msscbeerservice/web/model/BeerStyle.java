b43fbaf63bc9a9f0d37309c9f4ade363a8b9ea00
Ari Marin <ari.marin@oscar.fi>
2022-03-05 19:49:39 +0200

    Adding web data model. Closes #2.

---
+++ src/main/java/fi/oscar/msscbeerservice/web/model/BeerStyle.java
@@ -0,0 +1,8 @@
+package fi.oscar.msscbeerservice.web.model;
+
+/**
+ * Created by Ari on 05.03.2022
+ */
+public enum BeerStyle {
+	LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
